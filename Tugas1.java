805cbb2f76b0190ac77d593145e1315472780fd0
khairulfuad09 <kfuad9977@gmail.com>
2022-09-07 22:20:29 +0800

    menambahkan tugas 1

---
+++ Tugas1.java
@@ -0,0 +1,9 @@
+public class Tugas1 {
+    public static void main(String[] args){
+        System.out.println("Nama : Khairul Fuad");
+        System.out.println("Nim : 2110131310002");
+	System.out.println("Alamat : Jl. S. Adam. Komp. Mahligai. Rumah. No. 102");
+	System.out.println("Hobi : rebahan");
+	System.out.println("matkul : pemweb");
+}
+}
